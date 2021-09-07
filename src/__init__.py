e42b48ddab71093cbe0ed059d28b3ce6a97c0f50
jramos <jrdcasa>
2021-09-07 15:13:21 +0200

    Update

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,7 @@ from typing import Optional, List, TextIO
 
 def main(argv: Optional[List[str]] = None, stream: Optional[TextIO] = None) -> int:
 
-    from .sphinx_gui3 import Main
+    from .sphinx_gui3 import main_app
 
     with contextlib.ExitStack() as ctx:
-        return Main().execute(ctx)
+        return main_app().execute(ctx)
